d86c7f639a1873498f416b153d848e457945e091
ImMei-c <yuluMuMei@163.com>
2025-08-12 20:50:20 +0800

    first commit

---
+++ src/main/java/com/meilu/GitIdeaTest01Application.java
@@ -0,0 +1,13 @@
+package com.meilu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIdeaTest01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitIdeaTest01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/meilu/GitIdeaTest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meilu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIdeaTest01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
